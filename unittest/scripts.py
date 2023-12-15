3f3a0df0dd4be7307b4a0ce1da5e10beb2c052fc
rikurampanen <150280854+rikurampanen@users.noreply.github.com>
2023-12-15 13:07:48 +0200

    add comments
    Added short comments to functions, to describe what they do.

--- unittest/scripts.py
+++ unittest/scripts.py
@@ -1,20 +1,26 @@
+# Adds two values together
 def add(a, b):
     return a + b
 
+# Subtracts second value from the first
 def subtract(a, b):
     return a - b
 
+# Multiplies two values
 def multiply(a, b):
     return a * b
 
+# Divides first value by the second (throws error if second value is 0)
 def divide(a, b):
     if b == 0:
         raise ValueError("Division by zero is not allowed")
     return a / b
 
+# Checks if a number is even
 def is_even(num):
     return num % 2 == 0
 
+# Checks if a number is prime
 def is_prime(num):
     if num <= 1:
         return False
@@ -23,18 +29,23 @@ def is_prime(num):
             return False
     return True
 
+# Greets a person with their name
 def greet(name):
     return "Hello, " + name + "!"
 
+# Reverses a given string
 def reverse_string(string):
     return string[::-1]
 
+# Finds the maximum value in a list of numbers
 def find_max(nums):
     return max(nums)
 
+# Finds the minimum value in a list of numbers
 def find_min(nums):
     return min(nums)
 
+# Counts the number of vowels in a given text
 def count_vowels(text):
     vowels = "aeiouAEIOU"
     count = 0
@@ -43,9 +54,9 @@ def count_vowels(text):
             count += 1
     return count
 
+# Calculates the factorial of a number
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
-    
